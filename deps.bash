209c35d00e5807a5c792cdf175ab81f062e4337e
Aalok Shah <aalok@shah.ws>
2011-06-25 23:11:04 -0400

    Use GOROOT_FINAL rather than GOROOT

--- deps.bash
+++ deps.bash
@@ -7,36 +7,35 @@ OUT="Make.deps"
 TMP="Make.deps.tmp"
 
 if [ -f $OUT ] && ! [ -w $OUT ]; then
-        echo "$0: $OUT is read-only; aborting." 1>&2
-        exit 1
+	echo "$0: $OUT is read-only; aborting." 1>&2
+	exit 1
 fi
 
 # Get list of directories from Makefile
 dirs=$(sed '1,/^DIRS=/d; /^$/,$d; s/\\//g' Makefile)
-dirs2=$(sed '1,/^DIRS=/d; /^$/,$d; s/\\//g' $GOROOT/src/pkg/Makefile)
+dirs2=$(sed '1,/^DIRS=/d; /^$/,$d; s/\\//g' ${GOROOT_FINAL}/src/pkg/Makefile)
 dirpat=$(echo $dirs $dirs2 | sed 's/ /|/g; s/.*/^(&)$/')
 
 for dir in $dirs; do (
-        cd $dir || exit 1
+	cd $dir || exit 1
 
-        sources=$(sed -n 's/\.go\\/.go/p' Makefile)
-        sources=$(ls $sources 2> /dev/null)  # remove .s, .c, etc.
+	sources=$(sed -n 's/\.go\\/.go/p' Makefile)
+	sources=$(ls $sources 2> /dev/null)  # remove .s, .c, etc.
 
-        deps=$(
-                sed -n '/^import.*"/p; /^import[ \t]*(/,/^)/p' $sources /dev/null |
-                cut -d '"' -f2 |
-                egrep "$dirpat" |
-                grep -v "^$dir\$" |
-                sed 's/$/.install/' |
-                sort -u
-        )
+	deps=$(
+		sed -n '/^import.*"/p; /^import[ \t]*(/,/^)/p' $sources /dev/null |
+		cut -d '"' -f2 |
+		egrep "$dirpat" |
+		grep -v "^$dir\$" |
+		sed 's/$/.install/' |
+		sort -u
+	)
 
-        echo $dir.install: $deps
+	echo $dir.install: $deps
 ) done > $TMP
 
 for dir in $dirs2; do (
-  echo $dir.install: \${GOROOT}/pkg/\${GOOS}_\${GOARCH}/${dir}.a
+  echo $dir.install: \${GOROOT_FINAL}/pkg/\${GOOS}_\${GOARCH}/${dir}.a
 ) done >> $TMP
 
 mv $TMP $OUT
-
